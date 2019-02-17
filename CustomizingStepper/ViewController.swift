1fa54c2b8cf11a59789f6f26d41f63c2a3e6c1a9
정기욱 <rldnr56@me.com>
2019-02-17 12:57:14 +0900

    프로그래밍 코드를 이용하여 스테퍼를 루트뷰에 추가.

--- CustomizingStepper/ViewController.swift
+++ CustomizingStepper/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // CSStepper 객체 정의
+        let stepper = CSStpper()
+        stepper.frame = CGRect(x: 30, y: 100, width: 130, height: 30)
+        self.view.addSubview(stepper)
     }
 
 
